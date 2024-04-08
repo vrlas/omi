8c0f68aca3f005f3308f7045a8307ffca29be4c1
sineava <1690150504@qq.com>
2024-04-08 17:36:49 +0800

    fix:修复性能问题

--- src/views/other/img/index.vue
+++ src/views/other/img/index.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import img from './image.png'
 
-const [imgRef, videoRef, src, current] = [ref(null), ref(null), ref(null), ref(0)]
+const [imgRef, videoRef, src, current, isInit] = [ref(null), ref(null), ref(null), ref(0), ref(true)]
 let interval
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
@@ -19,10 +19,10 @@ const loadModels = async () => {
   if (dects.length) {
     const box = dects[0].alignedRect.box
     position.value = {
-      width: box.width * ratio,
-      height: box.height * ratio,
+      width: box.width * ratio * 1.1,
+      height: box.height * ratio * 1.1,
       left: box.left * ratio,
-      top: box.top * ratio
+      top: box.top * ratio * 0.8 // 修正系数
     }
   } else {
     position.value = {}
@@ -30,22 +30,29 @@ const loadModels = async () => {
 }
 
 // 开启人脸检测
-const face = () => {
+const face =  async () => {
   current.value = current.value === 1 ? 0 : 1
-  Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
-  ]).then(() => {
-    const constraints = { audio: false, video: { width: 1280, height: 720 } };
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(mediaStream => {
-        videoRef.value.srcObject = mediaStream
-        videoRef.value.onloadedmetadata = () => {
-          interval = setInterval(loadModels,100)
-        }
-    })
+  if (current.value !== 1) {
+    clearInterval(interval)
+    return
+  }
+  if (isInit.value) {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    ])
+    console.log(1)
+    isInit.value = false
+  }
+  const constraints = { audio: false, video: { width: 1280, height: 720 } };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(mediaStream => {
+      videoRef.value.srcObject = mediaStream
+      videoRef.value.onloadedmetadata = () => {
+        interval = setInterval(loadModels,100)
+      }
   })
 }
 
@@ -57,7 +64,7 @@ onUnmounted(() => {
 <template>
   <div class="relative t-card h-full">
     <el-button :type="current === 1 && 'primary'" @click="face">视频换狗头</el-button>
-    <div class="h-full translate-y-2">
+    <div v-if="current === 1" class="h-full translate-y-2">
       <video class="relative w-[800px]" autoplay ref="videoRef" @load="loadModels" />
       <img class="relative hidden" :src="src" ref="imgRef" />
       <div v-if="position.width"
