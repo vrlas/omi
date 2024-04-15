0f32d68e4c5d136d52e904ede39cd1308fce46b2
sineava <1690150504@qq.com>
2024-04-15 11:51:23 +0800

    feat:使用hash路由,页面非死链

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '@/views/login.vue'
 import Home from '@/views/home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
